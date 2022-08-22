cf0cd536446115df05a7a7a6e9b4b1b10418e233
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-22 10:30:09 +0300

    converts array to string

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length== 1
+         array.join()
+    elsif array.length == 2
+         array.join(" and ")
+    elsif array.length>2
+    last = array[-1]
+    array.pop
+     array.push("and #{last}")
+      array.join(", ")
+    end
 end
+oxford_comma(["fiddleheads","okra","kohlrabi"])
